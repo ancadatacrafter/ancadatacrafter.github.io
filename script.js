400132cc3fa725e5f7413a6340a4a2ad41cb91bb
ancadatacrafter <anca.datacrafter@gmail.com>
2024-09-16 18:04:12 +0200

    first test page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.querySelector('h1').addEventListener('click', function() {
-  alert('You clicked the heading!');
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
 });
